1390bb7463d396e267729da6754ce743351a6d0b
信鑫-King <45808948@qq.com>
2016-09-27 12:56:19 +0800

    提前体验下
    提前体验下

---
+++ applet-album/server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    port: '9993',
+    ROUTE_BASE_PATH: '/applet',
+
+    cosAppId: '10066543',
+    cosSecretId: 'AKIDqbBZUJMQhnZH1azAE7oKfrrfH1rY7Sfh',
+    cosSecretKey: 'znibMzQlyzolbFiz87vbAaoqvseEkAPT',
+    cosFileBucket: 'album',
+};
